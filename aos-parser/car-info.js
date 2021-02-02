33c04dc31aee2bbe9fa23c4419905351915f4e2d
Никита Корнилов <nikkorfed@gmail.com>
2021-02-02 12:26:13 +0500

    Вернул headless режим

--- aos-parser/car-info.js
+++ aos-parser/car-info.js
@@ -15,7 +15,6 @@ let getCarInfo = async (vin) => {
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: false,
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 900 });
