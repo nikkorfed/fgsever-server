c5959802af2eef0eb775c15bce6d259b2aa0fcee
Никита Корнилов <nikkorfed@gmail.com>
2021-12-04 10:29:05 +0300

    Add 'survey' route.

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  return next();
+});
+
 app.use("/aos-parser", require("./aos-parser"));
 app.use("/upgrade-calculator", require("./upgrade-calculator"));
+app.use("/survey", require("./survey"));
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}...`));
