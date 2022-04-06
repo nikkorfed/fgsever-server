4838d117be932c3ceaf871a155388db66cad815c
Никита Корнилов <nikkorfed@gmail.com>
2022-04-06 18:22:36 +0300

    Add logger middleware.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("module-alias/register");
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const { cors } = require("./middleware");
 const routes = require("./routes");
 
+app.use(morgan("dev"));
 app.use(cors);
 app.use(routes);
 
