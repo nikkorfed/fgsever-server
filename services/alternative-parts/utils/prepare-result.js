0c31e23107ff797638e4b32e77cfbc4e3613ab21
Никита Корнилов <nikkorfed@gmail.com>
2022-05-19 21:16:01 +0300

    Compare parts by description when adding.

--- services/alternative-parts/utils/prepare-result.js
+++ services/alternative-parts/utils/prepare-result.js
@@ -1,4 +1,6 @@
 const slugify = require("slugify");
+const { compareTwoStrings: similarity } = require("string-similarity");
+
 const sortParts = require("./sort-parts");
 const filterParts = require("./filter-parts");
 
@@ -7,26 +9,27 @@ let prepareResult = (parts, config) => {
 
   for (let part of parts) {
     let { name, description, number, price, shipping, from } = part;
-    let key = slugify(`${name} ${number.replace(/\s/g, "")}`, { lower: true });
+
+    let brand = name;
+    let type = description.split(" ")[0].toLowerCase();
+    let key = slugify(`${brand} ${type}`, { lower: true });
 
     if (number === config.originalNumber) {
       key = "original";
       name = "Оригинал";
     }
 
-    if (description?.match(/угол|углем/i)) {
-      name += ", угольный";
-    }
+    if (description?.match(/угол|углем/i)) name += ", угольный";
+    name = shipping ? `${name} (Доставка ${shipping})` : name;
+    price = price * 1.3;
 
-    if (!price || result[key]) continue;
+    let isSimilarPart = ([_, part]) => part.brand === brand && similarity(part.type, type) > 0.5;
+    let [similarKey, similarPart] = Object.entries(result).find(isSimilarPart) ?? [];
+    let isSimilarCheaper = similarPart?.price <= price;
 
-    result[key] = {
-      name: shipping ? `${name} (Доставка ${shipping})` : name,
-      description,
-      number,
-      price: price * 1.3,
-      from,
-    };
+    if (!price || isSimilarCheaper) continue;
+
+    result[similarKey ?? key] = { brand, name, description, type, number, price, from };
   }
 
   return sortParts(filterParts(result, config));
