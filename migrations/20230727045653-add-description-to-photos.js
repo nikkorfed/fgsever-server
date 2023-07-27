facb9a72c5f3b7e63ddfc629b6ed0a36271dd37e
Никита Корнилов <nikkorfed@gmail.com>
2023-07-27 10:21:16 +0500

    Add description column to photos.

---
+++ migrations/20230727045653-add-description-to-photos.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require("sequelize");
+
+exports.up = async (query, Sequelize) => {
+  await query.addColumn("photos", "description", { type: Sequelize.STRING });
+};
+
+exports.down = async (query, Sequelize) => {
+  await query.removeColumn("photos", "description");
+};
--- models/photo.js
+++ models/photo.js
@@ -6,6 +6,7 @@ const Photo = db.define("photo", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   url: { type: DataTypes.STRING, allowNull: false },
   workGuid: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
 });
 
 module.exports = Photo;
