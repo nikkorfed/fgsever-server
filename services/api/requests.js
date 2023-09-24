2f23ac59a5f1ed7ab9940c554dd1d688b1c6753e
Никита Корнилов <nikkorfed@gmail.com>
2023-09-25 02:23:46 +0500

    Send notifications for car wash request and test drive responses.

--- services/api/requests.js
+++ services/api/requests.js
@@ -1,9 +1,24 @@
 const utils = require("~/utils");
 
 const { Request } = require("../../models");
+const { notifications } = require("../../services/api");
+const { odata } = require("../../api");
 
 exports.create = async (body) => {
-  return await Request.create(body);
+  const request = await Request.create(body);
+
+  const work = await odata.getWork(request.workGuid);
+  const [car] = await odata.cars([work.carGuid]);
+
+  if (request.type === "carWash") {
+    await notifications.sendToMasters({
+      type: "requestCarWash",
+      title: "Нужна мойка",
+      body: `Заказчик запросил мойку автомобиля ${car.name} по заказ-наряду № ${work.number}.`,
+    });
+  }
+
+  return request;
 };
 
 exports.getAll = async (query) => {
@@ -18,6 +33,26 @@ exports.getById = async (guid, query) => {
 
 exports.updateById = async (guid, body) => {
   const request = await Request.findByPk(guid);
+
+  const work = await odata.getWork(request.workGuid);
+  const [car] = await odata.cars([work.carGuid]);
+
+  if (request.type === "testDrive" && body.status === "approved") {
+    await notifications.sendToMasters({
+      type: "approveTestDrive",
+      title: "Тест-драйв разрешен",
+      body: `Заказчик разрешил тестовую поездку на автомобиле ${car.name} по заказ-наряду № ${work.number}.`,
+    });
+  }
+
+  if (request.type === "testDrive" && body.status === "rejected") {
+    await notifications.sendToMasters({
+      type: "rejectTestDrive",
+      title: "Тест-драйв не нужен",
+      body: `Заказчик попросил не проводить тестовую поездку на автомобиле ${car.name} по заказ-наряду № ${work.number}.`,
+    });
+  }
+
   return await request.update(body);
 };
 
--- services/api/work-approvals.js
+++ services/api/work-approvals.js
@@ -1,17 +1,18 @@
 const utils = require("~/utils");
 
-const { WorkApproval, Work } = require("../../models");
+const { WorkApproval } = require("../../models");
 const { notifications } = require("../../services/api");
 const { odata } = require("../../api");
 
 exports.create = async (body) => {
   const workApproval = await WorkApproval.create(body);
   const work = await odata.getWork(workApproval.guid);
+  const [car] = await odata.cars([work.carGuid]);
 
   await notifications.sendToMasters({
     type: "addWorkApproval",
     title: "Работы согласованы",
-    body: `По заказ-наряду № ${work.number} заказчиком были согласованы указанные работы.`,
+    body: `По заказ-наряду № ${work.number} для ${car.name} заказчиком были согласованы указанные работы.`,
   });
 
   return workApproval;
