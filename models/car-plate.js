95ccfdf16127448ea36d97783ab5c989b23cd62a
Никита Корнилов <nikkorfed@gmail.com>
2024-05-07 19:17:17 +0500

    Add default value to car plate's guid.

--- models/car-plate.js
+++ models/car-plate.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 const db = require("../db");
 
 const CarPlate = db.define("carPlate", {
-  guid: { type: DataTypes.UUID, primaryKey: true },
+  guid: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   value: { type: DataTypes.STRING, allowNull: false },
   organization: { type: DataTypes.STRING, allowNull: false },
   source: { type: DataTypes.STRING, allowNull: false },
