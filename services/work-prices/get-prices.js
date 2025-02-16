a21e290572c197635c4c9e82be4a25181eb7508d
Никита Корнилов <nikkorfed@gmail.com>
2025-02-16 21:30:01 +0500

    Calculate work prices with norm hour.

--- services/work-prices/get-prices.js
+++ services/work-prices/get-prices.js
@@ -1,7 +1,18 @@
 const fs = require("fs/promises");
 
+let preparePrices = (prices, normHour) => {
+  for (const key in prices) {
+    if (typeof prices[key] === "object") prices[key] = preparePrices(prices[key], normHour);
+    else if (typeof prices[key] === "number" && prices[key] < 10) prices[key] = prices[key] * normHour;
+  }
+
+  return prices;
+};
+
 let getPrices = async (series) => {
-  const prices = JSON.parse(await fs.readFile(__dirname + "/prices.json"));
+  const input = JSON.parse(await fs.readFile(__dirname + "/prices.json"));
+  const prices = preparePrices(input, input.normHour);
+
   const result = series ? prices[series] : prices;
   return result;
 };
