887e4e36bebe4b8dd1544324d80fd24bea45d62c
Никита Корнилов <nikkorfed@gmail.com>
2025-04-25 11:33:28 +0500

    Send GetStocks request in Major Auto API.

--- services/parts/major-auto-api.js
+++ services/parts/major-auto-api.js
@@ -10,8 +10,10 @@ let searchInMajorAutoAPI = catchError(async (numbers, config = {}) => {
 
   // Запрос запчастей
   const client = await soap.createClientAsync(url);
+  const stocksResponse = await client.GetStocksAsync({ request: { Authority: { ConsumerID: identificator } } });
+  console.log("STOCKS:", stocksResponse[0]);
   const partsResponse = await client.GetAvailabilityAsync({
-    request: { Authority: { ConsumerID: identificator }, Rows: [{ Row: { PartNo: "11428575211" } }] },
+    request: { Authority: { ConsumerID: identificator }, Options: {}, Rows: [{ Row: { PartNo: "11428575211" } }] },
   });
   const [partsResult] = partsResponse;
   console.log("RESULT:", partsResult);
