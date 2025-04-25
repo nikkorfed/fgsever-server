195e2284b294a2a2c8a9f6375c348a83a9bf33f6
Никита Корнилов <nikkorfed@gmail.com>
2025-04-25 11:36:00 +0500

    Update console logs in Major Auto API.

--- services/parts/major-auto-api.js
+++ services/parts/major-auto-api.js
@@ -11,12 +11,13 @@ let searchInMajorAutoAPI = catchError(async (numbers, config = {}) => {
   // Запрос запчастей
   const client = await soap.createClientAsync(url);
   const stocksResponse = await client.GetStocksAsync({ request: { Authority: { ConsumerID: identificator } } });
-  console.log("STOCKS:", stocksResponse[0]);
+  console.log(stocksResponse[0].GetStocksResult);
   const partsResponse = await client.GetAvailabilityAsync({
     request: { Authority: { ConsumerID: identificator }, Options: {}, Rows: [{ Row: { PartNo: "11428575211" } }] },
   });
   const [partsResult] = partsResponse;
-  console.log("RESULT:", partsResult);
+  console.log(partsResult.GetAvailabilityResult);
+  console.log(partsResult.GetAvailabilityResult.Rows);
 
   // Оригинальные запчасти
   const originalParts = [];
